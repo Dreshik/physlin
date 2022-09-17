0bc91e9fd0ef9c371145f14c6a48c659a92ac31e
Vlados <allaods97@gmail.com>
2022-09-17 19:52:12 +0700

    physlinsh: добавлен вывод физического адреса
    При вызове physlin.sh {phys_addr} {val}, по адресу
    {phys_addr} будет записано значение {val}.

--- physlin.sh
+++ physlin.sh
@@ -9,9 +9,22 @@ function print_reg_read_val {
 	cat $kmod_path
 }
 
+function write_val {
+	physical_addr=$1
+	value=$2
+	echo "$physical_addr w $value" > $kmod_path
+}
+
 if [ $args_num -eq 1 ]
 then
 	physical_addr=$1
 	print_reg_read_val $physical_addr
 	exit 0
+fi
+
+if [ $args_num -eq 2 ]
+then
+	physical_addr=$1
+	value=$2
+	write_val "$physical_addr" "$value"
 fi
